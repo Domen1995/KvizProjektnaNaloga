ec6e3ff04551033b3565b49612a0cf77ea0c34bf
Domen1995 <tapewormerbinkosti@gmail.com>
2022-05-17 17:00:52 +0200

    meniPodrocij

--- application/views/vprasanje_in_odg/zacetek_vadbene_igre.php
+++ application/views/vprasanje_in_odg/zacetek_vadbene_igre.php
@@ -9,10 +9,10 @@
 <h3>Tule vaši odgovori ne bodo vplivali na točke ter vas uvrstili na rang listo, tudi če ste prijavljeni.</h3>
 <p>Izberite želeno področje vprašanja: </p>
 
-<ul>
-	<li><a id="podrocjaVpr" href="<?php echo site_url('Vadbena/random_question_podrocja/Knjizevnost_tekstovni'); ?>">Književnost</a></li>
-	<li><a id="podrocjaVpr" href="<?php echo site_url('Vadbena/random_question_podrocja/Zgodovina_tekstovni'); ?>">Zgodovina</a></li>
-	<li><a id="podrocjaVpr" href="<?php echo site_url('Vadbena/random_question_podrocja/Zabava_tekstovni'); ?>">Zabava</a></li>
-	<li><a id="podrocjaVpr" href="<?php echo site_url('Vadbena/random_question_podrocja/Geografija_tekstovni'); ?>">Geografija</a></li>
+<ul id="podrocjaVpr">
+	<li><a href="<?php echo site_url('Vadbena/random_question_podrocja/Knjizevnost_tekstovni'); ?>">Književnost</a></li>
+	<li><a href="<?php echo site_url('Vadbena/random_question_podrocja/Zgodovina_tekstovni'); ?>">Zgodovina</a></li>
+	<li><a href="<?php echo site_url('Vadbena/random_question_podrocja/Zabava_tekstovni'); ?>">Zabava</a></li>
+	<li><a href="<?php echo site_url('Vadbena/random_question_podrocja/Geografija_tekstovni'); ?>">Geografija/znanost</a></li>
 </ul>
 </div>
