01fccbde75b5228c5275285332a6506a687ddec7
Domen1995 <tapewormerbinkosti@gmail.com>
2022-05-18 21:57:44 +0200

    mm

--- application/views/uporabniski_obrazci/prijava.php
+++ application/views/uporabniski_obrazci/prijava.php
@@ -1,9 +1,16 @@
+<?php
+	if(isset($neveljavniPodatki))
+	{
+		?> <h2 style="color: grey; text-align: center; font-family: tahoma"> <?php echo $neveljavniPodatki; ?> </h2>
+	<?php }
+?>
+
 <div id="prijava">
 <form action="https://www.studenti.famnit.upr.si/~89181150/Kviz/CodeIgniter/index.php/uporabniki/prijavi" method="post" accept-charset="utf-8">
 <label><span>Vaš vzdevek: </span></label><br><input class="uporabniskiPodatki" type="text" name="vzdevek" value=""  />
-<br>
+<br><br>
 <label><span>Vaše geslo: </span></label>
 	<br><input class="uporabniskiPodatki" type="password" name="geslo" value=""  />
-<br>
-<input type="submit" name="submit" value="Prijavi me!"  />
+<br><br>
+<input id="gumbOddajVpr" type="submit" name="submit" value="Prijavi me!"  />
 </form></div>
--- application/views/uporabniski_obrazci/registracija.php
+++ application/views/uporabniski_obrazci/registracija.php
@@ -1,8 +1,17 @@
 <br>
+<?php
+	if(isset($neveljavenEnaslov))
+	{
+		?> <h2 style="color: grey; text-align: center; font-family: tahoma"> <?php echo $neveljavenEnaslov; ?> </h2>
+	<?php } elseif(isset($neveljavniPodatki))
+	{
+		?> <h2 style="color: grey; text-align: center; font-family: tahoma"> <?php echo $neveljavniPodatki; ?> </h2>
+	<?php }
+?>
 <div id="prijava">
 <form action="https://www.studenti.famnit.upr.si/~89181150/Kviz/CodeIgniter/index.php/Uporabniki/registriraj" method="post" accept-charset="utf-8">
 <label><span>Vaš bodoči vzdevek: </span></label><br><input type="text" name="vzdevek" value=""  />
-<br><label><span>Domislite se še gesla: </span></label><br><input type="password" name="geslo" value=""  />
-<br><label><span>Pa nam zaupajte še svoj e-naslov: </span></label><br><input type="text" name="enaslov" value=""  />
-<br><input type="submit" name="submit" value="Registracija!"  />
+<br><br><label><span>Domislite se še gesla: </span></label><br><input type="password" name="geslo" value=""  />
+<br><br><label><span>Pa nam zaupajte še svoj e-naslov: </span></label><br><input type="text" name="enaslov" value=""  />
+<br><br><input id="gumbOddajVpr" type="submit" name="submit" value="Registracija!"  />
 </form></div>
