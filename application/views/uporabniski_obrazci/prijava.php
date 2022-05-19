c85f57531c3532fd88dc8eafd24eb5dce8ce375d
Domen1995 <tapewormerbinkosti@gmail.com>
2022-05-19 11:11:49 +0200

    space

--- application/views/uporabniski_obrazci/prijava.php
+++ application/views/uporabniski_obrazci/prijava.php
@@ -8,7 +8,7 @@
 <div id="prijava">
 <form action="https://www.studenti.famnit.upr.si/~89181150/Kviz/CodeIgniter/index.php/uporabniki/prijavi" method="post" accept-charset="utf-8">
 <label><span>Vaš vzdevek: </span></label><br><input class="uporabniskiPodatki" type="text" name="vzdevek" value=""  />
-<br><br>
+<br>
 <label><span>Vaše geslo: </span></label>
 	<br><input class="uporabniskiPodatki" type="password" name="geslo" value=""  />
 <br><br>
